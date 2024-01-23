8dceb40b72733d9cb2bfa3c9efc55cd0487047fb
Hana Vu <vuhau.hsph@gmail.com>
2024-01-23 17:17:44 +1030

    config atlas database connection

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,7 +2,11 @@ import mongoose from "mongoose";
 async function connect() {
   try {
     await mongoose.connect(
-      "mongodb+srv://hauvu:VQ4TaeY54pM77j4M@cluster.y0rfjqb.mongodb.net/f8_education_dev"
+      "mongodb+srv://hauvu:VQ4TaeY54pM77j4M@cluster.y0rfjqb.mongodb.net/f8_education_dev?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
     );
     console.log("Connect successfully");
   } catch (error) {
