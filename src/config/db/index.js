cae22b2327a145e7d5302330768b4701f5358c94
Hana Vu <vuhau.hsph@gmail.com>
2024-01-23 16:45:51 +1030

    move database to mongodb atlas

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,7 +1,9 @@
 import mongoose from "mongoose";
 async function connect() {
   try {
-    await mongoose.connect("mongodb://127.0.0.1/f8_education_dev");
+    await mongoose.connect(
+      "mongodb+srv://hauvu:VQ4TaeY54pM77j4M@cluster.y0rfjqb.mongodb.net/f8_education_dev"
+    );
     console.log("Connect successfully");
   } catch (error) {
     console.log("Connect failure");
